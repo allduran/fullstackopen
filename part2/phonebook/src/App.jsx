ae0f733eebc3b19578f05f533039d155e579b710
duran <cheshire.dd96@gmail.com>
2025-02-11 17:51:12 +0100

    finished part2b exercises - 2.6_2.10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("enter name");
+  const [newNumber, setNewNumber] = useState("enter number");
+  const [searchName, setSearchName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {/* Search input */}
+      <Filter filter={searchName} handleFilterChange={handleSearchChange} />
+
+      {/* Add person form */}
+      <h3>Add a new</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
